94f69adf26521dfb0eb2e5b716cf5ae140ac157f
mukhammadali-shamsidinov <firebase1228@gmail.com>
2024-08-01 18:45:57 +0300

    first commit

---
+++ main.py
@@ -0,0 +1,21 @@
+import vonage
+
+
+client = vonage.Client(key="505d4adc", secret="UnLrCd53sMSkN30p")
+sms = vonage.Sms(client)
+
+
+responseData = sms.send_message(
+    {
+        "from": "Muhammadali",
+        "to": "998919226296",
+        "text": "Salom manga Ishoning",
+    }
+)
+
+print(responseData["messages"])
+
+if responseData["messages"][0]["status"] == "0":
+    print("Message sent successfully.")
+else:
+    print(f"Message failed with error: {responseData['messages'][0]['error-text']}")
